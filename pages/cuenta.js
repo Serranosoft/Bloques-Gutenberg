352ecdbccbdbed089ef7ae5e48d5854b68cfe028
Manuel Serrano <manuel.serrano943@gmail.com>
2021-06-12 17:42:29 +0200

    Fixed last commit bug

--- pages/cuenta.js
+++ pages/cuenta.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import styled from '@emotion/styled'
 import { DBContext } from "../components/Firebase/UserDAO";
 import { AuthContext } from '../components/Firebase/AuthDAO';
